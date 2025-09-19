e943f06823ee23c22d1592df63f22a7bb424030f
Jawdat Eissa <realmadred0@gmail.com>
2025-09-19 08:28:59 -0700

    setup husky

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,23 +1,23 @@
-import { useState, useEffect } from 'react'
-import { Button } from '@/components/ui/button'
-
+import { useState, useEffect } from 'react';
+import { Button } from '@/components/ui/button';
 
 function App() {
- const [message, setmessage] = useState();
+   const [message, setmessage] = useState();
 
- useEffect(() => {
-   fetch('/api/hello')
-     .then(response => response.json())
-     .then(data => setmessage(data.message));
- },[]);
+   useEffect(() => {
+      fetch('/api/hello')
+         .then((response) => response.json())
+         .then((data) => setmessage(data.message));
+   }, []);
 
- return (
-   <div className="App flex flex-col items-center justify-center min-h-screen bg-background">
-     <p className = "font-bold p-5 text-3xl">{message || 'Loading message...'}</p>
-     <Button >Click Me</Button>
-   </div>
- );
-
+   return (
+      <div className="App flex flex-col items-center justify-center min-h-screen bg-background">
+         <p className="font-bold p-5 text-3xl">
+            {message || 'Loading message...'}
+         </p>
+         <Button>Click Me !</Button>
+      </div>
+   );
 }
 
-export default App
+export default App;
